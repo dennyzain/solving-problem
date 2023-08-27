09aadc52104f2e4a8952c27f2f73578698135622
dennyzain <abbasdenny24@gmail.com>
2023-08-27 13:09:59 +0700

    feat: solve findOdd challenge

--- codewars/FindOdd.ts
+++ codewars/FindOdd.ts
@@ -1,3 +1,15 @@
 export default function FindOdd(input: number[]): number {
-  return 0;
+  const temp: { [key: string]: number } = {};
+  let result = 0;
+  for (const number of input) {
+    if (temp[number] !== undefined) {
+      temp[number] += 1;
+    } else {
+      temp[number] = 1;
+    }
+  }
+  for (const key in temp) {
+    if (temp[key] % 2 !== 0 || temp[key] == 1) result = Number(key);
+  }
+  return result;
 }
